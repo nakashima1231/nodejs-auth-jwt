eeba6826173a5746209cc326d7141dd8645869cd
Gabriel <gabrielmnakashima2@gmail.com>
2026-02-27 21:47:47 -0300

    primeiro commit

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const conexao = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
